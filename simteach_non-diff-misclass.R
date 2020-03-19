bce44e6619ba56b69d88700e8c810222a2f2e3e4
jerudolph13 <53434512+jerudolph13@users.noreply.github.com>
2020-03-19 12:09:40 -0400

    Add files via upload

--- simteach_non-diff-misclass.R
+++ simteach_non-diff-misclass.R
@@ -7,12 +7,13 @@
 #
 # Author: Jacqueline E. Rudolph
 #
-# Required packages: ggplot2
+# Required packages: ggplot2, tidyverse, mvtnorm
 #
-# Last Updated: 05 feb 2020
+# Last Updated: 11 mar 2020
 #
 #######################################################################################################
 
+##Set your working directory and load in necessary packages
 
 setwd("your_working_directory")
 
@@ -38,42 +39,51 @@ results <- data.frame(
 
 ##Run simulation  
 
-reps <- 10000
-n <- 1000
+reps <- 10000 #The number of simulations
+n <- 1000     #Sample size within each simulation
 
+#We carry out the simulations using the following function
 simloop <- function(r, n) {
   
   set.seed(r)
   results$rep <- r
   
-  #Confounders (2 continuous)
-  m <- rmvnorm(n, mean=rep(0, 2))
+  #Create 2 continuous confounders (M1 and M2)
+  #Both are normally distributed with mean 0 and s.d. 1
+  m <- rmvnorm(n, mean=rep(0, 2)) 
   
-  #Exposure affected by confounders
+  #Create binary exposure, P(A=1)=0.5
+  #A affected by M1 and M2: both with log(OR) of log(2)
   a <- rbinom(n, 1, 1/(1 + exp(-(-log(1/0.5 - 1) + log(2)*m[ , 1] + log(2)*m[ , 2]))))
   
-  #Outcome affected by exposure and confounders
-  y <- rnorm(n, 10 + 2*a + 2*m[ , 1] + 2*m[ , 2], 6)
+  #Continuous outcome affected by exposure and confounders
+  #Normally distributed with mean determined by formula below and s.d. 6
+  #Intercept of 10; mean difference of 2 for A, M1, and M2
+  y <- rnorm(n, mean=(10 + 2*a + 2*m[ , 1] + 2*m[ , 2]), sd=6)
   
-  #Exposure with non-differential misclassification
-  error_a <- rnorm(n)
-  a2 <- ifelse(error_a > qnorm(0.95), 1 - a, a)
+  #Exposure with NDM
+  #We select 15% of the sample to have misclassified A
+  error_a <- rnorm(n) #We use error_a to determine misclassification in anticipation of dependency below
+  a2 <- ifelse(error_a > qnorm(0.85), 1 - a, a) #Misclassified exposure
   
-  #Confounder 1 with non-differential misclassification
+  #Induce NDM of M1 that is correlated with error in exposure
+  #because misclassified variables both affected by error_a
   m2 <- m
-  m2[ , 1]  <- m[ , 1] - 0.9*error_a
+  m2[ , 1]  <- m[ , 1] - 0.9*error_a #Mismeasured M1, with extent of error dependent on error_a
   
-  #Scenario 1: No misclassification
+  #Scenario 1: Linear model under no misclassification
   results$delta1 <- summary(glm(y ~ a + m[ , 1] + m[ , 2], family=gaussian(link="identity")))$coefficients[2]
   
-  #Scenario 2: Misclassification of binary exposure
+  #Scenario 2: Linear model under misclassification of binary exposure
   results$delta2 <- summary(glm(y ~ a2 + m[ , 1] + m[ , 2], family=gaussian(link="identity")))$coefficients[2]
   
-  #Scenario 3: Dependent misclassification of binary exposure and continuous confounder
+  #Scenario 3: Linear model under dependent misclassification of binary exposure and continuous confounder
   results$delta3 <- summary(glm(y ~ a2 + m2[ , 1] + m2[ , 2], family=gaussian(link="identity")))$coefficients[2]
   
   return(results)
 }
+
+##Run the above function "reps" times, where reps is the number of simulations
 
 all.res <- lapply(1:reps, function(x) {simloop(x, n)})
 all.res <- do.call(rbind, all.res)
@@ -81,16 +91,19 @@ all.res <- do.call(rbind, all.res)
 
 ##Summarize results
 
-pos.error2 <- sum((all.res$delta2 - all.res$delta1) > 0)/reps
-pos.error3 <- sum((all.res$delta3 - all.res$delta1) > 0)/reps
-
-summ.res <- all.res %>% 
-  summarise(avg.delta1=mean(delta1), 
-            avg.delta2=mean(delta2), 
-            avg.delta3=mean(delta3)) 
-
-summ.res$bias2 <- summ.res$avg.delta2 - 2
-summ.res$bias3 <- summ.res$avg.delta3 - 2
+  #Proportion of simulations that had an error in the mean difference greater than the null
+  pos.error2 <- sum((all.res$delta2 - all.res$delta1) > 0)/reps #Comparing scenario 2 to scenario 1
+  pos.error3 <- sum((all.res$delta3 - all.res$delta1) > 0)/reps #Comparing scenario 3 to scenario 1
+  
+  #Get the average mean difference across simulations for each scenario
+  summ.res <- all.res %>% 
+    summarise(avg.delta1=mean(delta1), 
+              avg.delta2=mean(delta2), 
+              avg.delta3=mean(delta3)) 
+  
+  #Compare the average mean difference to the true mean difference of 2
+  summ.res$bias2 <- summ.res$avg.delta2 - 2
+  summ.res$bias3 <- summ.res$avg.delta3 - 2
 
 
 ##Visualize results
--- simteach_p-value.R
+++ simteach_p-value.R
@@ -7,10 +7,11 @@
 #
 # Required packages: ggplot2
 #
-# Last Updated: 31 jan 2020
+# Last Updated: 11 mar 2020
 #
 #######################################################################################################
 
+##Set your working directory and load in necessary packages
 
 setwd("your_working_directory")
 
@@ -25,31 +26,36 @@ for (package in packages) {
 
 ##Set up the simulation
 
-n <- 1000
-reps <- 10000
-set.seed(123)
-rd <- rep(NA,reps)
+reps <- 10000       #Number of permutations
+n <- 1000           #Number of individuals in each sample
 
-  #Simulate randomized treatment
+set.seed(123)
+rd <- rep(NA,reps)  #Vector to hold risk difference results
+
+  #Simulate randomized treatment, with P(R=1)=0.5
   r <- rbinom(n, 1, 0.5)
 
   #Simulate binary outcome affected by treatment
+  #P(Y=1)=0.1 and log OR for R is 0.75
   y <- rbinom(n, 1, 1/(1 + exp(-(-log(1/0.1 - 1) - 0.75*(0.5) + 0.75*r))))
 
-  #True effect and its p-value
+  #Estimate the RD for R and its p-value
   res <- glm(y ~ r, family=gaussian(link="identity"))
   rd_est <- summary(res)$coefficients[2]
   rd_pvalue <- summary(res)$coefficients[8]
 
-  #Permute exposure
+  #Permute exposure "reps" number of times
   for (i in 1:reps){
     set.seed(123 + i)
-    u <- runif(n)
-    r_new <- r[order(u)]
+    u <- runif(n)         #For each individual take a draw from a uniform distribution
+    r_new <- r[order(u)]  #Re-order the individuals on the basis of that draw
+    
+    #Using permuted exposure and original outcome, estimate the RD and save its value in the rd vector
     rd[i] <- summary(glm(y ~ r_new, family=gaussian(link="identity")))$coefficients[2]
   }
   
-  #Permutation p-value
+  #Calculate the one-sided permutation p-value, by taking the proportion
+  #of permutations that had a RD greater than the RD from the original sample
   prop <- sum(abs(rd) > rd_est)/reps
 
   
@@ -75,10 +81,10 @@ thm <- theme_classic() +
     #Two-sided p-value
     pdf("pval2.pdf",height=5,width=5)
     ggplot() +
-      geom_histogram(aes(abs(rd), stat(density)), bins=34, color="blue", fill="lightblue") +
+      geom_histogram(aes(abs(rd), stat(density)), bins=36, color="blue", fill="lightblue") +
       geom_vline(xintercept=rd_est, color="black", size=1) +
-      geom_label(aes(x=-0.01, y=56, label="B"), size=8, label.size=0.5) +
-      xlab("Absolute Value of the Risk Difference") + scale_x_continuous(expand=c(0, 0), limits=c(-0.016, 0.07)) + 
-      ylab("Density") + scale_y_continuous(expand=c(0, 0), limits=c(0, 60)) + thm
+      geom_label(aes(x=-0.06, y=46.5, label="B"), size=8, label.size=0.5) +
+      xlab("Absolute Value of the Risk Difference") + scale_x_continuous(expand=c(0, 0), limits=c(-0.07, 0.07)) + 
+      ylab("Density") + scale_y_continuous(expand=c(0, 0), limits=c(0, 50)) + thm
     dev.off()
 
