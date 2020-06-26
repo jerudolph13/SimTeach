cf2f022ad56e08de564526bf17e7e6b4fef5df53
jerudolph13 <53434512+jerudolph13@users.noreply.github.com>
2020-06-26 10:45:33 -0400

    Add files via upload
    Changes made as a result of the 2nd revision of the paper

--- simteach_p-value.R
+++ simteach_p-value.R
@@ -1,25 +1,20 @@
 
 #######################################################################################################
 #
-# Purpose: To show how simulation/permutation can be used to demonstrate the definition of a p-value
+# Purpose: To show how simulation can be used to demonstrate the definition of a p-value
 #
 # Author: Jacqueline E. Rudolph
 #
 # Required packages: ggplot2
 #
-# Last Updated: 11 mar 2020
+# Last Updated: 09 Jun 2020
 #
 #######################################################################################################
 
-##Set your working directory and load in necessary packages
-
-setwd("your_working_directory")
+##Load in necessary packages
 
 packages <- c("ggplot2")
 for (package in packages) {
-  if (!require(package, character.only=T, quietly=T)) {
-    install.packages(package,repos='http://lib.stat.cmu.edu/R/CRAN') 
-  }
   library(package, character.only=T)
 }
 
@@ -36,7 +31,7 @@ rd <- rep(NA,reps)  #Vector to hold risk difference results
   r <- rbinom(n, 1, 0.5)
 
   #Simulate binary outcome affected by treatment
-  #P(Y=1)=0.1 and log OR for R is 0.75
+  #P(Y=1)=0.1 and the OR for R is exp(0.75)=2.12
   y <- rbinom(n, 1, 1/(1 + exp(-(-log(1/0.1 - 1) - 0.75*(0.5) + 0.75*r))))
 
   #Estimate the RD for R and its p-value
@@ -44,20 +39,26 @@ rd <- rep(NA,reps)  #Vector to hold risk difference results
   rd_est <- summary(res)$coefficients[2]
   rd_pvalue <- summary(res)$coefficients[8]
 
-  #Permute exposure "reps" number of times
+  #Estimate the distribution of RDs under the null hypothesis that RD=0 
+    #Randomly assign a new exposure to each individual, still with P(R'=1)=0.5
+    #For this new exposure, estimate the RD on the original outcome
   for (i in 1:reps){
-    set.seed(123 + i)
-    u <- runif(n)         #For each individual take a draw from a uniform distribution
-    r_new <- r[order(u)]  #Re-order the individuals on the basis of that draw
-    
-    #Using permuted exposure and original outcome, estimate the RD and save its value in the rd vector
+    #Draw new randomized exposure
+    r_new <- rbinom(n, 1, 0.5)
+
+    #Using new exposure and original outcome, estimate the RD and save its value in the rd vector
     rd[i] <- summary(glm(y ~ r_new, family=gaussian(link="identity")))$coefficients[2]
   }
   
-  #Calculate the one-sided permutation p-value, by taking the proportion
-  #of permutations that had a RD greater than the RD from the original sample
-  prop <- sum(abs(rd) > rd_est)/reps
-
+  #Calculate the one-sided  p-value, by taking the proportion of reps
+  #that had a RD greater than the RD from the original sample
+  prop1 <- sum(rd > rd_est)/reps
+  
+  #Calculate the two-sided p-value, by taking the proportion of reps
+  #that had a RD more extreme than the RD from the original sample
+    #In other words, the absolute value of the RD was greater than the original RD
+  prop2 <- sum(abs(rd) > rd_est)/reps
+  
   
 ##Visualize the results
 
@@ -69,9 +70,8 @@ thm <- theme_classic() +
   )
 
     #One-sided p-value
-    pdf("pval1.pdf",height=5,width=5)
     ggplot() +
-      geom_histogram(aes(rd, stat(density)), bins=36, color="blue", fill="lightblue") +
+      geom_histogram(aes(rd, stat(density)), bins=36, color="gray", fill="lightgray") +
       geom_vline(xintercept=rd_est, color="black", size=1) +
       geom_label(aes(x=-0.06, y=21.5, label="A"), size=8, label.size=0.5) +
       xlab("Risk Difference") + scale_x_continuous(expand=c(0, 0), limits=c(-0.07, 0.07)) +  
@@ -79,9 +79,8 @@ thm <- theme_classic() +
     dev.off()
 
     #Two-sided p-value
-    pdf("pval2.pdf",height=5,width=5)
     ggplot() +
-      geom_histogram(aes(abs(rd), stat(density)), bins=36, color="blue", fill="lightblue") +
+      geom_histogram(aes(abs(rd), stat(density)), bins=36, color="gray", fill="lightgray") +
       geom_vline(xintercept=rd_est, color="black", size=1) +
       geom_label(aes(x=-0.06, y=46.5, label="B"), size=8, label.size=0.5) +
       xlab("Absolute Value of the Risk Difference") + scale_x_continuous(expand=c(0, 0), limits=c(-0.07, 0.07)) + 
